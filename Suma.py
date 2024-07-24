35941ea4d4d1477f592aaaa1d842eef06a829a4a
Ocupado777 <abel.gomez.tecnologicoliceocristiano.edu.ec>
2024-07-23 22:13:26 -0500

    Suma

---
+++ Suma.py
@@ -0,0 +1,10 @@
+number1 = int(input("ingrese el primer numero: "))
+number2 = int(input("ingrese el segundo numero: "))
+
+sum = number1 + number2
+print(sum)
+
+number3 = int(input("ingrese el tercer numero"))
+
+multi = sum * number3
+print(multi)
