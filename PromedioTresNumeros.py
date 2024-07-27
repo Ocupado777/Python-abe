f1597d27d4adc1d07987fcf37bb6dcf1a36eccaa
Ocupado777 <abel.gomez.tecnologicoliceocristiano.edu.ec>
2024-07-26 19:20:22 -0500

    Promediodenumeros

---
+++ PromedioTresNumeros.py
@@ -0,0 +1,9 @@
+N1 = int(input("Ingrese el primer número: "))
+N2 = int(input("Ingrese el segundo número: "))
+N3 = int(input("Ingrse el tercer número: "))
+
+suma = N1 + N2 + N3     
+print(suma)
+ 
+divi = suma / 3 
+print(divi)
